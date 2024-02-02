b6c35e9c42a93875d8c0eb1093f7ef9fb1cd03cd
Ahmed Hamouda <ahmedabdelrazzak1001010@gmail.com>
2024-02-02 06:40:47 -0800

    Added

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -45,8 +45,19 @@ function ensureUniqueNumber(model, fieldName, callback) {
 
 const createPdfBuffer = async (html) => {
 	const browser = await puppeteer.launch({
-		headless: "new", // Opt into the new headless mode
+		headless: true,
+		args: [
+			"--no-sandbox",
+			"--disable-setuid-sandbox",
+			"--disable-dev-shm-usage",
+			"--disable-accelerated-2d-canvas",
+			"--no-first-run",
+			"--no-zygote",
+			"--single-process", // <- this one doesn't works in Windows
+			"--disable-gpu",
+		],
 	});
+
 	const page = await browser.newPage();
 	await page.setContent(html, { waitUntil: "networkidle0" });
 	const pdfBuffer = await page.pdf({ format: "A4" });
